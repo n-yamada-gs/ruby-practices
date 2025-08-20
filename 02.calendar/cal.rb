8baabfe9a2de983f83a4cfaa5c94d0ede123a8c1
nyamada <n.yamada@genbasupport.com>
2025-08-20 17:50:45 +0900

    == trueを削除しました

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -3,7 +3,7 @@ require 'optparse'
 require 'date'
 
 # 今日の日付を取得
-today =Date.today
+today = Date.today
 
 # 引数を指定しない場合に、今月・今年を返す
 month = today.month
@@ -11,22 +11,21 @@ year = today.year
 
 # 年・月を受け取り、表示したい月の最初の日と最後の日を取得する
 opt = OptionParser.new
-opt.on('-m [VAL]') {|v| month = v.to_i}
-opt.on('-y [VAL]') {|v| year = v.to_i}
+opt.on('-m [VAL]') { |v| month = v.to_i }
+opt.on('-y [VAL]') { |v| year = v.to_i }
 opt.parse!(ARGV)
 first_date = Date.new(year, month, 1)
 last_date = Date.new(year, month, -1)
 
-puts "#{month}月 #{year}".center(20)  
-puts "日 月 火 水 木 金 土"
+puts "#{month}月 #{year}".center(20)
+puts '日 月 火 水 木 金 土'
 
-print "   " * first_date.wday
+print '   ' * first_date.wday
 
 (first_date..last_date).each do |date|
-  if date.saturday? == true
-    puts "#{date.day}".rjust(2)
+  if date.saturday?
+    puts date.day.to_s.rjust(2)
   else
     print "#{date.day} ".rjust(3)
   end
 end
-
