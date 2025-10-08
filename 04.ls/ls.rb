321061ca1a659ba06f866350481192aa0b259930
nyamada <n.yamada@genbasupport.com>
2025-10-08 13:45:52 +0900

    Hashに計算結果を保持するように変更しました。

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -34,51 +34,76 @@ def output(files, file_matrix)
   end
 end
 
-def print_in_short_format(files, row_number, file_matrix)
+def print_in_short_format(files)
   file_matrix = format_files(files, row_length(files))
   output(files, file_matrix)
 end
 
+# -lオプションに必要なデータたち
+def get_long_format_datas(files)
+  files.map do |file|
+    stat = File.stat(file)
+    {
+      name: file,
+      file_type: File.ftype(file),
+      nlink: stat.nlink,
+      mode_number: stat.mode.to_s(8)[-3..],
+      user_id: stat.uid,
+      group_id: stat.gid,
+      byte_size: stat.size,
+      time: stat.mtime,
+      total: stat.blocks
+    }
+  end
+end
+raw_datas = get_long_format_datas(files)
+# ユーザー名とグループ名を取得し、ハッシュを追加する
+def add_user_and_group_names(raw_datas)
+  raw_datas.map do |data|
+    user_name = Etc.getpwuid(data[:user_id]).name
+    group_name = Etc.getgrgid(data[:group_id]).name
+    data.merge(user_name: user_name, group_name: group_name)
+  end
+end
+long_format_datas = add_user_and_group_names(raw_datas)
+
 # ハードリンクの数の桁数
-def get_max_hard_link_digit_length(files)
-  hard_links = files.map { |file| File.stat(file).nlink }
-  max_hard_link_digit_length = hard_links.max.to_s.size
+def get_max_hard_link_digit_length(long_format_datas)
+  hard_links = long_format_datas.map { |data| data[:nlink] }
+  hard_links.max.to_s.size
 end
+
 # ユーザー名の最大文字数
-def get_user_name_length(files)
-  user_names = files.map do |file|
-    user_id = File.stat(file).uid
-    Etc.getpwuid(user_id).name
-  end
-  max_user_name_length = user_names.max_by(&:size).size
+def get_max_user_name_length(long_format_datas)
+  long_format_datas.map { |data| data[:user_name].size }.max
 end
+
 # グループ名の最大文字数
-def get_max_group_name_length(files)
-  group_names = files.map do |file|
-    group_id = File.stat(file).gid
-    Etc.getpwuid(group_id).name
-  end
-  max_group_name_length = group_names.max_by(&:size).size
+def get_max_group_name_length(long_format_datas)
+  long_format_datas.map { |data| data[:group_name].size }.max
 end
+
 # バイトサイズの桁数
-def get_max_bite_size_digit_length(files)
-  bite_size = files.map { |file| File.size(file) }
-  max_bite_size_digit_length = bite_size.max.to_s.size
+def get_max_byte_size_digit_length(long_format_datas)
+  byte_size = long_format_datas.map { |data| data[:byte_size] }
+  byte_size.max.to_s.size
 end
+
 # 「合計」の表示
-def get_total
-  total_block_size =  files.sum { |file| File.stat(file).blocks * 512 / 1024 }
-  puts "合計 #{total_block_size}"
+def get_total(long_format_datas)
+  long_format_datas.sum { |data| data[:total] * 512 / 1024 }
 end
+
 # ファイルタイプ（モード）を1文字に変換する
-def convert_file_type(file)
-  case File.ftype(file)
+def convert_file_type(data)
+  case data[:file_type]
   when 'file' then '-'
   when 'directory' then 'd'
   when 'link' then 'l'
   else '?'
   end
 end
+
 # パーミションの数字を記号表記に変換する
 def convert_mode_sign(mode_number)
   mode_number.each_char.map do |char|
@@ -96,34 +121,30 @@ def convert_mode_sign(mode_number)
   end
 end
 
-def print_in_long_format(files, max_hard_link_digit_length, max_user_name_length, max_group_name_length, max_bite_size_digit_length)
-  files.each do |file|
-    file_type = convert_file_type(file)
-    mode_number = File.stat(file).mode.to_s(8)[-3..]
+def print_in_long_format(long_format_datas, max_hard_link_digit_length, max_user_name_length, max_group_name_length, max_byte_size_digit_length)
+  long_format_datas.each do |data|
+    file_type = convert_file_type(data)
+    mode_number = data[:mode_number]
     mode_sign = convert_mode_sign(mode_number)
-
-    user_id = File.stat(file).uid
-    group_id = File.stat(file).gid
-
     print file_type
     print mode_sign.join
-    print " #{File.stat(file).nlink.to_s.rjust(max_hard_link_digit_length)}"
-    print " #{Etc.getpwuid(user_id).name.ljust(max_user_name_length)}"
-    print " #{Etc.getpwuid(group_id).name.ljust(max_group_name_length)}"
-    print " #{File.size(file).to_s.rjust(max_bite_size_digit_length)}"
-    print " #{File.stat(file).mtime.strftime('%m月 %d %R')}"
-    puts " #{file}"
+    print " #{data[:nlink].to_s.rjust(max_hard_link_digit_length)}"
+    print " #{data[:user_name].ljust(max_user_name_length)}"
+    print " #{data[:group_name].ljust(max_group_name_length)}"
+    print " #{data[:byte_size].to_s.rjust(max_byte_size_digit_length)}"
+    print " #{data[:time].strftime('%m月 %d %R')}"
+    puts " #{data[:name]}"
   end
 end
 
 if params[:l]
-  max_hard_link_digit_length = get_max_hard_link_digit_length(files)
-  max_user_name_length = get_user_name_length(files)
-  max_group_name_length = get_max_group_name_length(files)
-  max_bite_size_digit_length = get_max_bite_size_digit_length(files)
-  print_in_long_format(files, max_hard_link_digit_length, max_user_name_length, max_group_name_length, max_bite_size_digit_length)
+  total_block_size = get_total(long_format_datas)
+  puts "合計 #{total_block_size}"
+  max_hard_link_digit_length = get_max_hard_link_digit_length(long_format_datas)
+  max_user_name_length = get_max_user_name_length(long_format_datas)
+  max_group_name_length = get_max_group_name_length(long_format_datas)
+  max_byte_size_digit_length = get_max_byte_size_digit_length(long_format_datas)
+  print_in_long_format(long_format_datas, max_hard_link_digit_length, max_user_name_length, max_group_name_length, max_byte_size_digit_length)
 else
-  row_number = row_length(files)
-  file_matrix = format_files(files, row_number)
-  print_in_short_format(files, row_number, file_matrix)
+  print_in_short_format(files)
 end
